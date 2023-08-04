051353d8425db62450ad9dbbafab355acf56992a
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 10:00:22 +0700

    Method Perulangan Dengan Statement

---
+++ src/main/java/com/helloworld/helloworld/ForLoop_DenganStatement.java
@@ -0,0 +1,10 @@
+package com.helloworld.helloworld;
+
+public class ForLoop_DenganStatement {
+    public static void main(String[] args) {
+        for (var counter = 1; counter <= 10;) {
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        }
+    }
+}
