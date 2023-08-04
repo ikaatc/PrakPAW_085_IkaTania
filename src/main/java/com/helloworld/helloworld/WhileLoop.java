641740db8fd80187a8dffdc01740a572b148d39a
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 10:11:52 +0700

    Method While Loop

---
+++ src/main/java/com/helloworld/helloworld/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.helloworld.helloworld;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        while (counter <= 10) {
+            System.out.println("Perulangan Ke-" + counter);
+            counter++;
+        }
+    }
+}
