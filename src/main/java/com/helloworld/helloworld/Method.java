3cc3c827e3b40018db1a9d9bebb43f845de84148
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 10:47:10 +0700

    Method sayHelloWorld Method Function

---
+++ src/main/java/com/helloworld/helloworld/Method.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class Method {
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+
+    private static void sayHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
