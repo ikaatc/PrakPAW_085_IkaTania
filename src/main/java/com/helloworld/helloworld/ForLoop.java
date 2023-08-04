424b915b36a688191e7ee52175652ff9e8a130eb
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 09:53:37 +0700

    Method For Looping

---
+++ src/main/java/com/helloworld/helloworld/ForLoop.java
@@ -0,0 +1,9 @@
+package com.helloworld.helloworld;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("Perulangan Tanpa Henti");;
+        }
+    }
+}
