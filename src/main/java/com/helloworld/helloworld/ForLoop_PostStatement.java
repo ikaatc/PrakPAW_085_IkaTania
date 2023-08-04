8ca783c4c23495bbdedc614bc26530008fcdc41e
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 10:02:57 +0700

    Method For Loop Post Statement

---
+++ src/main/java/com/helloworld/helloworld/ForLoop_PostStatement.java
@@ -0,0 +1,9 @@
+package com.helloworld.helloworld;
+
+public class ForLoop_PostStatement {
+    public static void main(String[] args) {
+        for (var counter = 1; counter <= 10; counter++) {
+            System.out.println("Perulangan Ke-" + counter);
+        }
+    }
+}
