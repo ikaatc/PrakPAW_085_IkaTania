a6941a416b2d3459543572dc682db35af985de19
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 09:57:52 +0700

    Method Perulangan Dengan Kondisi

---
+++ src/main/java/com/helloworld/helloworld/ForLoop_DenganKondisi.java
@@ -0,0 +1,12 @@
+package com.helloworld.helloworld;
+
+public class ForLoop_DenganKondisi {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        for (; counter <= 30;) {
+            System.out.println("Perulangan Ke-" + counter);
+            counter++;
+        }
+    }
+}
