921ca76da56d9b1161f540d95b2760d05fadc55b
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-04 10:37:31 +0700

    Method Tanpa For Each

---
+++ src/main/java/com/helloworld/helloworld/TanpaForEach.java
@@ -0,0 +1,14 @@
+package com.helloworld.helloworld;
+
+public class TanpaForEach {
+    public static void main(String[] args) {
+        String[] array = {
+                "Ika", "Tania", "Chaerani",
+                "Yang", "Sangat", "Comel"
+        };
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
