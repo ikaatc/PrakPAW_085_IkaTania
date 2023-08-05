65e7d7e3ab7470bd3b82064ba10f1111e4431019
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-05 09:59:00 +0700

    Method Overloading

---
+++ src/main/java/com/helloworld/helloworld/MethodOverloading.java
@@ -0,0 +1,21 @@
+package com.helloworld.helloworld;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        sayHello();
+        sayHello("Ika");
+        sayHello("Ika", " Tania");
+    }
+
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    static void sayHello(String firstName) {
+        System.out.println("Hello " + firstName);
+    }
+
+    static void sayHello(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + lastName);
+    }
+}
