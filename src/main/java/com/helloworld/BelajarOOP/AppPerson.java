105d9d0029d079ce0bff6935afd4a0e80e95c5a1
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-08-11 11:31:14 +0700

    method constructor, memakai dan menggunakan

--- src/main/java/com/helloworld/BelajarOOP/AppPerson.java
+++ src/main/java/com/helloworld/BelajarOOP/AppPerson.java
@@ -2,19 +2,19 @@ package com.helloworld.BelajarOOP;
 
 public class AppPerson {
     public static void main(String[] args) {
-        var person = new Person();
+        var person = new Person("Ika", "Pky");
         // AppPerson person2 = new AppPerson();
 
         // AppPerson person3;
         // person3 = new AppPerson();
 
         person.name = "Ika";
-        person.address = "Pky";
+        // person.address = "Pky";
         // person.country = "Tidak Bisa Diubah"
 
-        System.out.println(person.name);
-        System.out.println(person.address);
-        System.out.println(person.country);
+        // System.out.println(person.name);
+        // System.out.println(person.address);
+        // System.out.println(person.country);
 
         person.sayHello("Tania");
     }
--- src/main/java/com/helloworld/BelajarOOP/Person.java
+++ src/main/java/com/helloworld/BelajarOOP/Person.java
@@ -5,6 +5,11 @@ public class Person {
     String address;
     final String country = "Indonesia";
 
+    Person(String paramName, String paramAddress) {
+        name = paramName;
+        address = paramAddress;
+    }
+
     void sayHello(String paramName) {
         System.out.println("Hello " + paramName + ", My Name is " + name);
     }
